8d718975599307887ed8e38ab1008582de81a511
Annie <annie2liang@gmail.com>
2021-11-22 21:07:07 -0600

    upating

--- firstPython.py
+++ firstPython.py
@@ -1,4 +1,20 @@
 if __name__ =='__main__':
+    
+    inrange_1=range(2,5)
+    
     n = int(input("Enter your number "))
-    if (n%2) != 0:
+    
+    if n%2 == 0:
+        
+        if n in inrange_1:
+            print("Not Weird")
+            
+        if n in range(6,20):
+            print("Weird")
+            
+        if n > 20:
+            print("Not Weird")
+        
+    else:
         print("Weird")
+        
