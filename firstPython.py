b41de3d838ac44de170cca43292b3b877fdfa279
Annie <annie2liang@gmail.com>
2021-11-25 20:11:46 -0600

    updating with comments

--- firstPython.py
+++ firstPython.py
@@ -1,17 +1,20 @@
 if __name__ =='__main__':
     
-    inrange_1=range(2,6)
+    # inrange_1=range(2,6)
     
-    n = int(input("Enter your number "))
+    # n = int(input("Enter your number "))
     
-    if n%2 != 0:
-        print("Weird")
+    # if n%2 != 0:
+    #     print("Weird")
     
-    if n in inrange_1 and n%2 == 0:
-        print("Not Weird")
+    # if n in inrange_1 and n%2 == 0:
+    #     print("Not Weird")
             
-    if n in range(6,21) and n%2 == 0:
-        print("Weird")
+    # if n in range(6,21) and n%2 == 0:
+    #     print("Weird")
             
-    if n > 20 and n%2 == 0:
-        print("Not Weird")
+    # if n > 20 and n%2 == 0:
+    #     print("Not Weird")
+        
+    print(3/5) #this prints float data types
+    print(3//5) #this prints integer data types
